b56f9dc674b17008c16158487b8583653f610eb6
jake <jpollard@ucsd.edu>
2020-08-18 15:00:58 -0700

    database stuff

--- db.py
+++ db.py
@@ -26,13 +26,23 @@ class Database:
 
   #this has not been checked
 	def query(self,queryString):
-		cursor = connection.cursor()
+		cursor = self.connection.cursor()
 		try:
 			cursor.execute(queryString)
 			connection.commit()
 			print(queryString)
 		except Error as e:
  			print("Error {} occured.".format(e))
+
+	def queryMany(self,string,parameters):
+		cursor = self.connection.cursor()
+		try:
+			#cursor.executemany(string,parameters)
+			#connection.commit()
+			print("{} with {} parameters".format(string,len(parameters)))
+		except Error as e:
+ 			print("Error {} occured.".format(e))
+
 
 	#Parse, clean and 'compress' data for storage in db
 	def loadCsvToDB(self,dbName,fileName):
@@ -73,29 +83,73 @@ class Database:
 		"poutcome": {"success": 'S', "failure": 'F', "other": 'O', "unknown": '?'},
 		#"y"' - CHAR - (Y)es, (N)o
 		"y": {"yes": 'Y', "no": 'N'}}
+
+		create_query = """ CREATE TABLE IF NOT EXISTS entries (
+											 id INT AUTO_INCREMENT,
+											 age TINYINT,
+											 job CHAR(1),
+											 maritial CHAR(1),
+											 education CHAR(1),
+											 isDefault CHAR(1),
+											 balance INT,
+											 housing CHAR(1),
+											 loan CHAR(1),
+											 contact CHAR(1),
+											 day TINYINT,
+											 month TINYINT,
+											 duration SMALLINT,
+											 campaign TINYINT,
+											 pdays SMALLINT,
+											 previous TINYINT,
+											 poutcome CHAR(1),
+											 y CHAR(1),
+											 PRIMARY KEY (id)) ENGINE = InnoDB
+											 """
+		#sql = "INSERT INTO likes ( user_id, post_id ) VALUES ( %s, %s )"
+		insert_query = """INSERT INTO entries ( age, job, maritial, education, 
+																						isDefault, balance, housing,
+																						loan, contact, day, month,
+																						duration, campaign, pdays,
+																						previous, poutcome, y )
+																	 VALUES ( %d, %c, %c, %c, 
+																	 					%c, %d, %c, 
+																						%c, %c, %d, %d, 
+																						%d, %d, %d, 
+																						%d, %d, %c,  )"""
 		with open(fileName) as fi:
 			reader = csv.reader(fi,delimiter=';')
 			firstRow = True
 
-
+			self.query(create_query)
+			#values for entries
+			params = []
 	
-      #['58;"management";"married";"tertiary";"no";2143;"yes";"no";"unknown";5;"may";261;1;-1;0;"unknown";"no"']
 			count = 0
 			for row in reader:
+				#get catagories from the first row of the file
 				if firstRow:
-					print(row)
+
       		#get first line of .csv, the catagories
 					catagories = [ item for item in row]
+
+
+
 					firstRow = False
+				#process data for following rows
 				else:
-					print("ROW: {}".format(count))
+					#print("ROW: {}".format(count))
+					#clear current entry
+					entry =  []
+					#process each field according to type
 					for i in range(len(catagories)):
-						print('catagory: {}'.format(catagories[i]))
 						if isinstance(dictionary[catagories[i]],dict):
-							print(dictionary[catagories[i]][row[i]])
+							entry.append(dictionary[catagories[i]][row[i]])
 						else:
-							print(int(row[i]))
+							entry.append(int(row[i]))
+
+					params.append(tuple(entry))
 				count+=1
+			self.queryMany(insert_query,params)
         
         
 
